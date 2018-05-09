dc1aec08011cc6568706e9edfbae2b007f25b082
Patrick <mesinapatrick@gmail.com>
2018-05-09 18:20:08 +0800

    Second Commit

--- pages/PictureStream.dart
+++ pages/PictureStream.dart
@@ -20,9 +20,50 @@ class PictureData extends StatefulWidget{
 }
 
 class PictureDataState extends State<PictureData> {
-
   
+  StreamController<Photo> streamController;
+  //To initialize the State
+  List<Photo> list = [];
+
+
+  @override 
+  void initState() {
+    super.initState();
+    streamController = StreamController.broadcast();
+
+    streamController.stream.listen((p) =>
+      setState(() 
+      => list.add(p)));
+
+    load(streamController);
+  
+
+  }
+
+  load (StreamController sc) async{
+    String url = "https://jsonplaceholder.typicode.com/photos";
+    
+    var client = new http.Client();
+
+    var request =  new http.Request('get', Uri.parse(url));
+
+
+    var streamRes = await client.send(request);
+
+
+  }
 }
 
-//changes
+
+
+class Photo{
+  final String title;
+  final String url;
+
+
+
+  Photo.fromJsonMap(Map map)
+    : title = map['title'],
+      url = map['url'];
+}
 
